7162d8182189cece97ae413afcc2b4ca16129fd8
Pranali <pranalilambat00@gmail.com>
2022-01-11 09:20:08 +0530

    Febbonacci

---
+++ FirstQue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6LogicalProblem
+{
+    class FirstQue
+    {
+        public void FabonacciSeries()
+        {
+            int a, b,number,i,c;
+            a = 0;
+            b = 1;
+            Console.WriteLine("Enter the number : ");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(a + " " + b + " ");
+            for (i = 2; i < number; i++)
+            {
+                c = a + b;
+                Console.Write(c + " ");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
