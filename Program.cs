1f68cd1d781457f202deba302268773cb91298af
Pranali <pranalilambat00@gmail.com>
2022-01-11 09:02:04 +0530

    Day6Problem

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day6LogicalProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FirstQue firstque = new FirstQue();
+            //firstque.FabonacciSeries();
+            //PerfectNumber perfectnumber = new PerfectNumber();
+            //perfectnumber.PerfectNum();
+            PrimeNumber primenumber = new PrimeNumber();
+            primenumber.PrimeNum();
+        }
+    }
+}
